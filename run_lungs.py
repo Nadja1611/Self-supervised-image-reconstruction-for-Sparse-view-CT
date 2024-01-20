69dc4f800fb8e2692fa40f3a60ee32b323fca836
Nadja1611 <61620034+Nadja1611@users.noreply.github.com>
2024-01-20 11:20:11 +0100

    Update run_lungs.py

--- run_lungs.py
+++ run_lungs.py
@@ -147,30 +147,11 @@ Images = resize(Images, (30,336,336))
 images = Images[:]
 
 
-def add_gaussian_noise(img, sigma):
-    img = np.array(img)
-    noise = np.random.normal(0, sigma, img.shape)/100
-    img = img + np.max(img)*noise
-    return torch.tensor(img)
-    
 
-def create_noisy_sinograms(images, angles_full, sigma):
-    # 0.1: Make geometry:
-    geo = ctgeo.Geometry.parallel_default_parameters(
-        image_shape=images.shape, number_of_angles=angles_full
-    )  # parallel beam standard CT
-    # 0.2: create operator:
-    op = ct.make_operator(geo)
-    # 0.3: forward project:
-    sino = op(torch.from_numpy(images))
-    sinogram_full = add_gaussian_noise(sino, sigma)
-    sinogram_full = torch.moveaxis(sinogram_full, -1, -2)
-    return np.asarray(sinogram_full.unsqueeze(1))
+
 
 ########################### N2I ###########################
 number_angles = 64
-sinograms = torch.tensor(create_noisy_sinograms(images, number_angles, 0))
-
 torch.manual_seed(0)
 
 maxi= torch.max(sinograms)
