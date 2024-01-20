f76f9a7bbbf36b2b855467a8ca0ee1d6c71332a2
Nadja1611 <61620034+Nadja1611@users.noreply.github.com>
2024-01-20 11:23:14 +0100

    Update run_lungs.py

--- run_lungs.py
+++ run_lungs.py
@@ -149,6 +149,25 @@ images = Images[:]
 
 
 
+    
+
+def create_sinograms(images, angles_full):
+    # 0.1: Make geometry:
+    geo = ctgeo.Geometry.parallel_default_parameters(
+        image_shape=images.shape, number_of_angles=angles_full
+    )  # parallel beam standard CT
+    # 0.2: create operator:
+    op = ct.make_operator(geo)
+    # 0.3: forward project:
+    sino = op(torch.from_numpy(images))
+    sinogram_full = torch.moveaxis(sinogram_full, -1, -2)
+    return np.asarray(sinogram_full.unsqueeze(1))
+
+########################### N2I ###########################
+number_angles = 16
+sinograms = torch.tensor(create_sinograms(images, number_angles))
+
+
 
 ########################### N2I ###########################
 number_angles = 64
